b88c8e01b89b99f13e7a783d4ac710718a987c06
Marlies Dens <marlies.dens@pxl.be>
2026-05-03 15:07:22 +0200

    try-catch en list<broodjes>

--- Broodjesservice/Broodje.cs
+++ Broodjesservice/Broodje.cs
@@ -17,13 +17,13 @@ namespace Broodjesservice
 			get { return _type; }
 			set
             {
-                if (Array.Exists(_allowedTypes, type => type.Equals(value, StringComparison.OrdinalIgnoreCase))
+                if (Array.Exists(_allowedTypes, type => type.Equals(value, StringComparison.OrdinalIgnoreCase)))
                 {
                     _type = value;
                 }
                 else
                 {
-                    throw new ArgumentException($"Ongeldige type {value}. De types zijn 'warm', 'koud', 'veggie' of 'speciaal'. ");
+                    throw new ArgumentException($"Ongeldige type {value}. De types zijn 'warm', 'koud', 'veggie' of 'speciaal'.");
                 }
             }
 		}
@@ -31,6 +31,11 @@ namespace Broodjesservice
         public decimal Price { get; set; }
 
         private static readonly string[] _allowedTypes = { "warm", "koud", "veggie", "speciaal" };
+
+        public static string[] AllowedTypes //string hierboven is private, enkel bruikbaar in de klasse. Deze public string[] is leesbaar in program-klasse (om de types niet zelf opnieuw te moeten typen in overzicht in program-klasse)
+        {
+            get { return _allowedTypes; }
+        }
 
         public Broodje(string name, string type, decimal price)
         {
--- Broodjesservice/Program.cs
+++ Broodjesservice/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Broodjesservice
+﻿using System.ComponentModel;
+
+namespace Broodjesservice
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List < Broodje > broodjes = new List<Broodje>();
+
+            Console.WriteLine("----BROODJESBESTELLING----");
+            Console.Write($"Geldige types: ");
+            foreach (string type in Broodje.AllowedTypes) 
+            {
+                Console.Write($"'{type}' ");
+            }
+
+            Console.WriteLine();
+            Console.ReadLine();
+
+            string inputName;
+            do
+            {
+                Console.WriteLine("Geef de naam van het broodje (of 'stop' om te eindigen):" +
+                    "");
+                inputName = Console.ReadLine();
+                if (inputName == "stop")
+                {
+                    break;
+                }
+                Console.WriteLine("Geef het type: ");
+                string inputType = Console.ReadLine();
+                Console.WriteLine("Geef de prijs: ");
+                decimal.TryParse(Console.ReadLine(), out decimal price);
+
+                try
+                {
+                    broodjes.Add(new Broodje(inputName, inputType, price));
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+
+                Console.ReadLine();
+
+            } while (inputName != "stop") ;
+
         }
     }
 }
