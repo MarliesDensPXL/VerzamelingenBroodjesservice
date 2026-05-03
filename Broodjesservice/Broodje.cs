57fc8d1a34f138b94c1bd324ba93a58fa0244db7
Marlies Dens <marlies.dens@pxl.be>
2026-05-03 14:17:15 +0200

    Add project files.

---
+++ Broodjesservice/Broodje.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Broodjesservice
+{
+    public class Broodje
+    {
+        public string Name { get; set; }
+
+		private string _type;
+
+		public string Type
+		{
+			get { return _type; }
+			set
+            {
+                if (Array.Exists(_allowedTypes, type => type.Equals(value, StringComparison.OrdinalIgnoreCase))
+                {
+                    _type = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Ongeldige type {value}. De types zijn 'warm', 'koud', 'veggie' of 'speciaal'. ");
+                }
+            }
+		}
+
+        public decimal Price { get; set; }
+
+        private static readonly string[] _allowedTypes = { "warm", "koud", "veggie", "speciaal" };
+
+        public Broodje(string name, string type, decimal price)
+        {
+            Name = name;
+            Type = type;
+            Price = price;
+        }
+
+    }
+}
---
+++ Broodjesservice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Broodjesservice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
