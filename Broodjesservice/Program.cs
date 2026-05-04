6eab2d602a9454dfc62423e53c21ce752ab37407
Marlies Dens <marlies.dens@pxl.be>
2026-05-04 09:38:45 +0200

    nieuwe types toevoegen aan dictionary & prijs aanpassen bij bestaande types

--- Broodjesservice/Program.cs
+++ Broodjesservice/Program.cs
@@ -7,6 +7,7 @@ namespace Broodjesservice
         static void Main(string[] args)
         {
             List < Broodje > broodjes = new List<Broodje>();
+            Dictionary<string, decimal> amountPerType = new Dictionary<string, decimal>();
 
             Console.WriteLine("----BROODJESBESTELLING----");
             Console.Write($"Geldige types: ");
@@ -35,14 +36,24 @@ namespace Broodjesservice
 
                 try
                 {
-                    broodjes.Add(new Broodje(inputName, inputType, price));
+                    broodjes.Add(new Broodje(inputName, inputType, price)); // niet eerst nieuw broodje maken en daarna toevoegen maar in één weg (dan moet ik geen zinloze variabele-naam voor het nieuwe broodje verzinnen)
                 }
                 catch (ArgumentException ae)
                 {
                     Console.WriteLine(ae.Message);
                 }
+                bool isExistingType = broodjes.Exists(b => b.Type == inputType); //nakijken of er al een broodje in de lijst staat van het nieuw ingevoerde type.
+                if (!isExistingType) //als type nog niet voorkomt in de lijst, nieuw item in dictionary aanmaken.
+                {
+                    amountPerType.Add(inputType, 0.0m); 
+                }
+                else
+                {
+                    amountPerType[inputType] += price;
+                }
 
-                Console.ReadLine();
+
+                    Console.ReadLine();
 
             } while (inputName != "stop") ;
 
