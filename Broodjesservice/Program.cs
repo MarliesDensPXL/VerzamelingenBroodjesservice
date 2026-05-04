fcca0dc94650481b94cf147ed62ca71a1313a698
Marlies Dens <marlies.dens@pxl.be>
2026-05-04 21:33:37 +0200

    zoeken in dictionary ipv in list broodjes

--- Broodjesservice/Program.cs
+++ Broodjesservice/Program.cs
@@ -38,24 +38,24 @@ namespace Broodjesservice
                 //nakijken of er al een broodje in de lijst staat van het nieuw ingevoerde type. (stond eerst ná try-catch maar daarin wordt broodje toegevoegd, dus uiteraard is bool dan altijd true).
                 // bool isExistingType = broodjes.Exists(b => b.Type == inputType); 
 
-                if (!broodjes.Exists(b => b.Type.Equals(inputType)))
+                if (!amountPerType.ContainsKey(inputType))
                 { 
                     try
                     {
                         broodjes.Add(new Broodje(inputName, inputType, price)); // niet eerst nieuw broodje maken en daarna toevoegen maar in één weg (dan moet ik geen zinloze variabele-naam voor het nieuwe broodje verzinnen)
                         amountPerType.Add(inputType, 0.0m);
-                        amountPerType[inputType] += price;
+                        // amountPerType[inputType] += price;
 
                     }
                     catch (ArgumentException ae)
                     {
                         Console.WriteLine(ae.Message);
+                        continue;
                     }
                 }
-                else
-                {
+               
                     amountPerType[inputType] += price;
-                }
+               
 
                     
                     Console.ReadLine();
